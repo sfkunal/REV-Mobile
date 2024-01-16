02ebc24973d471ced555b8bff9e5d877f10af1dd
Kunal Srivastava <kunal@masagroup.com>
2024-01-16 10:45:29 -0800

    logo ratio adjusted

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ export const config = {
   primaryColor: '#884EF3',
   primaryColorDark: '#884EF3',
   logoWidth: 160,
-  logoSizeRatio: 1, // height/width
+  logoSizeRatio: 0.4, // height/width
   shopifyUrl: 'https://rev.delivery/',
   shopifyStorefrontAccessToken: 'f41bc1d738f6e3af7fd7b3243a084d8c',
   instagramUsername: 'rev.delivery',
