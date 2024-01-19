d8547e9e187ad3aa6479ec3c342c88fc9cbf35a9
Kunal Srivastava <kunal@masagroup.com>
2024-01-19 01:47:24 -0800

    BUG: removing cart from bottom nav tab creates error

--- src/screens/TabNavigator.tsx
+++ src/screens/TabNavigator.tsx
@@ -7,7 +7,7 @@ import HomeStackNavigator from './StackNavigators/HomeStackNavigator';
 import SearchStackNavigator from './StackNavigators/SearchStackNavigator';
 import CartStackNavigator from './StackNavigators/CartStackNavigator';
 import ProfileStackNavigator from './StackNavigators/ProfileStackNavigator';
-import { BagIcon, HomeIcon, ProfileIcon, SearchIcon, StoreIcon } from '../components/shared/Icons';
+import { CartIcon, HomeIcon, ProfileIcon, SearchIcon, StoreIcon } from '../components/shared/Icons';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StackParamList } from '../types/navigation'
 import { useNavigationContext } from '../context/NavigationContext';
@@ -52,7 +52,7 @@ const TabNavigator = ({ navigation }: Props) => {
               return <SearchIcon size={18} color={color} />
             }
             if (route.name == 'Cart') {
-              return <BagIcon size={21} color={color} />
+              return <CartIcon size={21} color={color} />
             }
             if (route.name == 'Profile') {
               return <ProfileIcon size={22} color={color} />
@@ -63,7 +63,7 @@ const TabNavigator = ({ navigation }: Props) => {
         <Tab.Screen name="Home" component={HomeStackNavigator} />
         {/* <Tab.Screen name="Menu" component={MenuStackNavigator} /> */}
         <Tab.Screen name="Search" component={SearchStackNavigator} />
-        {/* <Tab.Screen name="Cart" component={CartStackNavigator} /> */}
+        <Tab.Screen name="Cart" component={CartStackNavigator} />
         <Tab.Screen name="Profile" component={ProfileStackNavigator} />
       </Tab.Navigator>
     </NavigationContainer>
